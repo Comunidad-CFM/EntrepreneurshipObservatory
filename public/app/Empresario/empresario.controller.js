f3d2d403421c980e2917535671f9e0dae314df70
jimenezgonzalezc <car_ed24@hotmail.es>
2016-04-18 21:11:47 -0600

    Ventana Empresario

--- public/app/Empresario/empresario.controller.js
+++ public/app/Empresario/empresario.controller.js
@@ -8,10 +8,20 @@
 	function EmpresarioController($scope, $http, $cookies, Auth) {
 		$scope.user = $cookies.getObject('session');
 		$scope.logOut = logOut;
+		$scope.tipoUsuario = false;
+		$scope.s = s;
 
 		function logOut() {
 			Auth.logOut();
 		}
+
+		function s () {
+			if($cookies.getObject('session').tipo === 'B'){
+				$scope.tipoUsuario = false;
+			}
+
+		}
+		s();
 	}
 
 })();
