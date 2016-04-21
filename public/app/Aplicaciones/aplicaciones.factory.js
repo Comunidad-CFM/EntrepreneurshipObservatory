112cdd43d94401a90937ce37d88b1b07b553cb3e
Fauricio Rojas <fauri-94@hotmail.com>
2016-04-20 18:57:50 -0600

    Asignar empresarios a las encuestas completado

--- public/app/Aplicaciones/aplicaciones.factory.js
+++ public/app/Aplicaciones/aplicaciones.factory.js
@@ -7,7 +7,9 @@
 
 	function AplicacionesFactory($http, $q) {
 		var factory = {
-			getForSurvey: getForSurvey
+			getForSurvey: getForSurvey,
+			store: store,
+			remove: remove
 		};
 
 		return factory;
@@ -30,6 +32,51 @@
 				defered.reject(err);
 			});
 
+			return defered.promise;
+		}
+
+		function store(entrepreneurs, idPeriodo, fecha, idEncuesta) {
+			var defered = $q.defer(),
+				data = {
+					entrepreneurs: entrepreneurs,
+					idPeriodo: idPeriodo,
+					fecha: fecha,
+					idEncuesta: idEncuesta
+				};
+
+			$http({
+				method: 'POST',
+				url: 'api/aplicaciones/store',
+				data: data
+			})
+			.success(function(response) {
+				defered.resolve(response);
+			})
+			.error(function(err) {
+				defered.reject(err);
+			});
+
+			return defered.promise;
+		}
+
+		function remove(aplications) {
+			var defered = $q.defer(),
+				data = {
+					aplications: aplications
+				};
+
+			$http({
+				method: 'POST',
+				url: 'api/aplicaciones/remove',
+				data: data
+			})
+			.success(function(response) {
+				defered.resolve(response);
+			})
+			.error(function(err) {
+				defered.reject(err);
+			});
+			
 			return defered.promise;
 		}
 	}
