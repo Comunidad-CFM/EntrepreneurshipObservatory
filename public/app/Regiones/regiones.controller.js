610495cd31ed3c5df552f6daccd7c7c306d6e92e
hiken13 <mancraft13@hotmail.es>
2016-05-13 21:24:02 -0600

    Trabajando en el mantenimiento de Regiones

--- public/app/Regiones/regiones.controller.js
+++ public/app/Regiones/regiones.controller.js
@@ -11,7 +11,22 @@
         .module('observatoryApp')
         .controller('RegionesController', RegionesController);
 
-    function RegionesController($scope) {    	
-       
+    function RegionesController($scope, RegionesFactory) {    	
+
+
+    	/*
+    	* Obtener la lista de regiones almacenadas en la base de datos y asignarla a la lista d eregiones del
+    	* scope.
+    	*/
+       function getRegiones () {
+       	 RegionesFactory.getAll()
+       	 	.then(function (response) {       	 		
+       	 		console.log(response);
+       	 	 	$scope.regiones = response;
+       	 });       	 	
+       }
+
+       getRegiones();
+
     }  	
 })();
