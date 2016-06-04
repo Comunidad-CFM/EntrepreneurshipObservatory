b9cb268d19513574ab26e99c65640685613836b2
hiken13 <mancraft13@hotmail.es>
2016-06-04 00:16:21 -0600

    Trabajando en el analisis de encuestas

--- public/app/Analisis/analisis.controller.js
+++ public/app/Analisis/analisis.controller.js
@@ -9,16 +9,42 @@
 	* @param {Object} Servicio que permite la unión entre el HTML y el controlador.
 	* @param {Object} Servicio que brinda funciones del analisis al controlador.
 	*/
-	function AnalisisController ($scope, AnalisisFactory) {		
-		$scope.get = get;
+	function AnalisisController ($scope, AnalisisFactory, IndicadoresFactory, SectoresFactory) {		
+		// $scope.get = get;
 
 		function get() {
 			AnalisisFactory.get(1)
 			.then(function(response) {
-				console.log(response);
+				return response;
+			})
+			.then(function (response) {
+				groupByEntrepreneur(response);
 			});
 		}
 
-		get();
+		function getIndicadores () {
+			IndicadoresFactory.getAll()
+				.then(function (response) {
+					$scope.indicadores = response;
+				});
+		}
+
+		function getSectores () {
+			SectoresFactory.getAll()
+				.then(function (response) {
+					$scope.sectores = response;
+				});
+		}
+
+	 	function groupByEntrepreneur(answers){
+			$scope.answers = AnalisisFactory.groupByEntrepreneur(answers);
+			console.log($scope.answers);
+		}
+
+	 	get();
+		getSectores();
+	 	getIndicadores();
+
+
 	}
 })();
--- public/app/Analisis/analisis.factory.js
+++ public/app/Analisis/analisis.factory.js
@@ -7,7 +7,9 @@
 
 	function AnalisisFactory($http, $q) {
 		var factory = {
-			get: get
+			get: get,
+			calculateICEBySector: calculateICEBySector,
+			groupByEntrepreneur: groupByEntrepreneur
 		};
 
 		return factory;
@@ -27,6 +29,53 @@
 			});
 
 			return defered.promise;
+		}
+
+		function calculateICEBySector(sector, entrepreneurs) {
+			
+		}
+
+		/*
+		*
+		* */
+		function destroyAnswer(answers, answer) {
+			answers = answers.filter(function(item) {
+				return answer !== item;
+			});
+
+			return answers;
+		}
+
+		/*
+		* */
+		function groupByEntrepreneur(answers){
+			var i = 0,
+				j = 0,
+				index = 0,
+				entrepreneurs = [];
+			console.log(answers);
+			for ( ; i < answers.length; i++) {
+
+				entrepreneurs.push({
+					id: answers[i].id,
+					answers: []
+				});
+
+				for ( ; j < answers.length; j++) {
+					if (entrepreneurs[index].id === answers[j].id) {
+						entrepreneurs[index].answers.push({
+							indicador: answers[j].nombre,
+							answer: answers[j].respuesta,
+						});
+						answers = destroyAnswer(answers, answers[j]);
+						j--;
+					}
+				}
+				i = -1;
+				j = 0;
+				index++;
+			}
+			return entrepreneurs;
 		}
 	}
 })();
